fdfcb0ed49ceefbfab75fc3f513afcc94d238d18
= <=>
2022-12-18 20:59:48 +0530

    company query added

--- controllers/products.js
+++ controllers/products.js
@@ -7,11 +7,14 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
   }
   console.log(queryObject);
   const products = await Product.find(queryObject);
