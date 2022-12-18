e991ff0064c270d6131edfcf8614cd3e68b10361
= <=>
2022-12-18 20:55:27 +0530

    query params refactored

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,14 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
   res.status(200).json({ products, nbHits: products.length });
 };
 
